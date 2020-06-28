7d5cdf9bd30e2257ff160681f23d9476bf8f12d9
ajpagente <alvininsg@gmail.com>
2020-06-28 23:18:15 +0800

    Refactor SystemCommand.fileInfo

--- Sources/Command/SystemCommand.swift
+++ Sources/Command/SystemCommand.swift
@@ -8,42 +8,8 @@ import Foundation
 
 public struct SystemCommand {
     public static func fileInfo(of filePath: String, with arguments: [String:String] = ["":""]) -> CommandOutput {
-        var appName     = ""
-
-        let tempDirectoryURL  = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
-        var tempExtractionURL: URL?
-
-        let fileManager = FileManager.default
-
-        let ipaURL = URL(fileURLWithPath: filePath, isDirectory: false)
-
-        do {
-            tempExtractionURL = try fileManager.createTemporaryDirectory(atPath: "ipa-extract")
-
-            unzip(ipaURL.path, to: tempExtractionURL!.path)
-
-            let resourceKeys = Set<URLResourceKey>([.nameKey, .isDirectoryKey])
-            let directoryEnumerator = fileManager.enumerator(at: tempExtractionURL!, includingPropertiesForKeys: Array(resourceKeys), options: .skipsHiddenFiles)!
-            
-            for case let fileURL as URL in directoryEnumerator {
-                guard let resourceValues = try? fileURL.resourceValues(forKeys: resourceKeys),
-                    let isDirectory = resourceValues.isDirectory,
-                    let name = resourceValues.name
-                    else {
-                        continue
-                }
-                
-                if isDirectory {
-                    if name.contains(".app") { 
-                        appName = name    
-                        break 
-                    }
-                }
-            }
-        } catch {
-            print("Failed extraction")
-            return CommandOutput(simple: [""], verbose: [""])
-        }
+        let tempExtractionURL = extractToTemporaryDirectory(filePath)
+        let appName           = getAppName(from: tempExtractionURL!)
 
         if let tempExtractionURL = tempExtractionURL {
             let tempPayloadURL = tempExtractionURL.appendingPathComponent("Payload")
@@ -61,7 +27,49 @@ public struct SystemCommand {
 
     @discardableResult
     public static func unzip(_ filePath: String, to extractPath: String) -> Bool {
-        let output = Process().execute("/usr/bin/unzip", arguments: ["-q", filePath, "-d", extractPath])
+        let _ = Process().execute("/usr/bin/unzip", arguments: ["-q", filePath, "-d", extractPath])
         return true
     }
+}
+
+extension SystemCommand {
+    private static func extractToTemporaryDirectory(_ filePath: String) -> URL? {
+        let fileManager = FileManager.default
+        let ipaURL = URL(fileURLWithPath: filePath, isDirectory: false)
+
+        do {
+            let tempExtractionURL = try fileManager.createTemporaryDirectory(atPath: "ipa-extract")
+            SystemCommand.unzip(ipaURL.path, to: tempExtractionURL.path)
+            return tempExtractionURL
+        } catch {
+            return nil
+        }
+    }
+
+    // A directory called <app name>.app is expected to be present in an ipa
+    private static func getAppName(from extractionURL: URL) -> String {
+        var appName = ""
+        let fileManager = FileManager.default
+        let resourceKeys = Set<URLResourceKey>([.nameKey, .isDirectoryKey])
+        let directoryEnumerator = fileManager.enumerator(at: extractionURL, includingPropertiesForKeys: Array(resourceKeys), options: .skipsHiddenFiles)!
+            
+        for case let fileURL as URL in directoryEnumerator {
+            guard let resourceValues = try? fileURL.resourceValues(forKeys: resourceKeys),
+                let isDirectory = resourceValues.isDirectory,
+                let name = resourceValues.name
+                else {
+                    continue
+                }
+                
+            if isDirectory {
+                if name.contains(".app") { 
+                    appName = name    
+                    break 
+                }
+            }
+        }
+
+        return appName
+    }
+    
 }
