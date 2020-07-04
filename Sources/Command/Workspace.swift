330892e65b4de5ef7c669e662310de6f592c6e4a
ajpagente <alvininsg@gmail.com>
2020-07-04 23:36:44 +0800

    Completed Workspace functions

--- Sources/Command/Workspace.swift
+++ Sources/Command/Workspace.swift
@@ -6,19 +6,21 @@
 
 import Foundation
 import Files
+import ZIPFoundation
 
 // Workspace creates folders for a command to use for its operations.
 public struct Workspace {
     public let baseFolder   : Folder
     public let outputFolder : Folder
     public let inputFolder  : Folder
-    private let tempFolder  : Folder
+    public let tempFolder  : Folder
 
     public enum Destination {
         case input
         case output
         case temporary
     }
+
     public init() throws {
         let randomFolder = UUID().uuidString
         baseFolder   = try Folder.temporary.createSubfolder(named: randomFolder)
@@ -27,22 +29,45 @@ public struct Workspace {
         tempFolder   = try baseFolder.createSubfolder(named: "tmp")
     }
 
+    /// Write file to chosen folder. A parameter to unzip files is provided.
     public func writeFile(_ file: File, to destination: Destination, decompress: Bool = false) throws {
-        switch destination {
-            case .input:
-                try file.copy(to: inputFolder)
-            case .output:
-                try file.copy(to: outputFolder)
-            case .temporary:
-                try file.copy(to: tempFolder)
+        if decompress == true {
+            let fileManager = FileManager()
+            let fileURL = URL(fileURLWithPath: file.path)
+            var destinationURL: URL
+            switch destination {
+                case .input:
+                    destinationURL = URL(fileURLWithPath: inputFolder.path)
+                case .output:
+                    destinationURL = URL(fileURLWithPath: outputFolder.path)
+                case .temporary:
+                    destinationURL = URL(fileURLWithPath: tempFolder.path)
+            }
+
+            try fileManager.unzipItem(at: fileURL, to: destinationURL)
+        } else {
+            switch destination {
+                case .input:
+                    try file.copy(to: inputFolder)
+                case .output:
+                    try file.copy(to: outputFolder)
+                case .temporary:
+                    try file.copy(to: tempFolder)
+            }
         }
     } 
 
-    // public func moveOutput(to destination: String) {
+    /// Copies the file located in the output folder to a folder.
+    public func copyFileFromOutput(named name: String, to folder: Folder) throws {
+        let file = try outputFolder.file(named: name)
+        try file.copy(to: folder)
+    }
 
-    // }
-
-    // public func wipe() {
-
-    // }
+    /// Permanently delete the contents of all folders.
+    /// The folders are not deleted so the workspace can be still be used.
+    public func emptyFolders() throws {
+        try inputFolder.empty(includingHidden: true)
+        try outputFolder.empty(includingHidden: true)
+        try tempFolder.empty(includingHidden: true)
+    }
 }
--- Tests/revampTests/WorkspaceTests.swift
+++ Tests/revampTests/WorkspaceTests.swift
@@ -5,12 +5,15 @@ import Files
 
 final class WorkspaceTests: XCTestCase {
     private var folder: Folder!
+    private var resourcesFolder: Folder!
 
     // MARK: - XCTestCase
     override func setUp() {
         super.setUp()
         folder = try! Folder.home.createSubfolderIfNeeded(withName: ".workspaceTest")
         try! folder.empty()
+
+        resourcesFolder = try! Folder(path: FileManager.default.currentDirectoryPath).subfolder(at: "Tests/Resources")
     }
 
     override func tearDown() {
@@ -20,27 +23,82 @@ final class WorkspaceTests: XCTestCase {
     
     // MARK: - Tests
 
-    func testWorkspaceCreate() throws {
+    func testCreate() throws {
         let workspace = try Workspace()
         XCTAssertEqual(workspace.outputFolder.name, "out")
         XCTAssertEqual(workspace.inputFolder.name, "in")
+        XCTAssertEqual(workspace.tempFolder.name, "tmp")
     }
 
-    func testWorkspaceWrite() throws {
+    func testWrite() throws {
         let workspace = try Workspace()
-        XCTAssertEqual(workspace.outputFolder.name, "out")
-        XCTAssertEqual(workspace.inputFolder.name, "in")
 
         let file = try folder.createFile(named: "test.txt")
         try file.write("content")
 
         try workspace.writeFile(file, to: .input)
         XCTAssertTrue(workspace.inputFolder.containsFile(named: "test.txt"))
+
+        try workspace.writeFile(file, to: .output)
+        XCTAssertTrue(workspace.outputFolder.containsFile(named: "test.txt"))
+
+        try workspace.writeFile(file, to: .temporary)
+        XCTAssertTrue(workspace.tempFolder.containsFile(named: "test.txt"))
     }
 
+    func testWriteWithDecompress() throws {
+        let zipFile   = try resourcesFolder.file(named: "tiny.ipa")
+        let workspace = try Workspace()
+
+        try workspace.writeFile(zipFile, to: .input, decompress: true)
+        XCTAssertTrue(workspace.inputFolder.containsSubfolder(named: "Payload"))
+
+        try workspace.writeFile(zipFile, to: .output, decompress: true)
+        XCTAssertTrue(workspace.outputFolder.containsSubfolder(named: "Payload"))
+
+        try workspace.writeFile(zipFile, to: .temporary, decompress: true)
+        XCTAssertTrue(workspace.tempFolder.containsSubfolder(named: "Payload"))
+
+        try workspace.emptyFolders()
+    }
+
+    func testCopyFile() throws {
+        let workspace = try Workspace()
+
+        let file = try folder.createFile(named: "test.txt")
+        try file.write("content")
+        try workspace.writeFile(file, to: .output)
+
+        let destinationFolder = try folder.createSubfolder(named: "target")
+        try workspace.copyFileFromOutput(named: "test.txt", to: destinationFolder)
+        XCTAssertTrue(destinationFolder.containsFile(named: "test.txt"))
+    }
+
+    func testEmptyFolders() throws {
+        let workspace = try Workspace()
+
+        let file = try folder.createFile(named: "test.txt")
+        try file.write("content")
+
+        try workspace.writeFile(file, to: .input)
+        try workspace.writeFile(file, to: .output)
+        try workspace.writeFile(file, to: .temporary)
+
+        XCTAssertTrue(workspace.inputFolder.containsFile(named: "test.txt"))
+        XCTAssertTrue(workspace.outputFolder.containsFile(named: "test.txt"))
+        XCTAssertTrue(workspace.tempFolder.containsFile(named: "test.txt"))
+
+        try workspace.emptyFolders()
+
+        XCTAssertFalse(workspace.inputFolder.containsFile(named: "test.txt"))
+        XCTAssertFalse(workspace.outputFolder.containsFile(named: "test.txt"))
+        XCTAssertFalse(workspace.tempFolder.containsFile(named: "test.txt"))
+    }
 
     static var allTests = [
-        ("testWorkspaceCreate", testWorkspaceCreate),
-        ("testWorkspaceWrite",  testWorkspaceWrite),
+        ("testCreate"      , testCreate),
+        ("testWrite"       , testWrite),
+        ("testCopyFile"    , testCopyFile),
+        ("testEmptyFolders", testEmptyFolders),
     ]
 }
