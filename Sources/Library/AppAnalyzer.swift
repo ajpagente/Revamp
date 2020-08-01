16712fe3a7e7fa329acb4b6be0f7a9d05d4836e9
ajpagente <alvininsg@gmail.com>
2020-08-01 22:18:18 +0800

    show info now shows profile UUID,expiry, and other info

--- Sources/Library/AppAnalyzer.swift
+++ Sources/Library/AppAnalyzer.swift
@@ -31,11 +31,12 @@ public struct AppAnalyzer {
         let signGroup = OutputGroup(lines: signInfo, header: "App Signature", 
                                     separator: ":", overrideMaxCount: appGroup.maxCount)
 
-        let profileCertificatesInfo = try getProfileCertificatesInfo(from: appFolders.first!.path)
-        let profileCertificatesGroup = OutputGroup(lines: profileCertificatesInfo, header: "Profile Certificates", 
+        let profileInfo = try getProfileInfo(from: appFolders.first!.path)
+        let profileGroup = OutputGroup(lines: profileInfo, header: "Profile Info", 
                                     separator: ":", overrideMaxCount: appGroup.maxCount)
         
-        return [appGroup, signGroup, profileCertificatesGroup]
+        let entitlements = try getEntitlements(from: appFolders.first!.path)
+        return [appGroup, signGroup, profileGroup]
     }
 
     private static func getSignInfo(from appPath: String) throws -> [String] {
@@ -58,7 +59,7 @@ public struct AppAnalyzer {
         return info
     }
 
-    private static func getProfileCertificatesInfo(from appPath: String) throws -> [String] {
+    private static func getProfileInfo(from appPath: String) throws -> [String] {
         let profileFile = try! Folder(path: appPath).file(named: "embedded.mobileprovision")
         let profileURL  = URL(fileURLWithPath: profileFile.path)
 
@@ -68,12 +69,33 @@ public struct AppAnalyzer {
         var info: [String] = []
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM dd,yyyy"
+
+        info.append("Profile Name: \(profile!.name)")
+        info.append("Profile UUID: \(profile!.UUID)")
+        info.append("App ID Name: \(profile!.appIDName)")
+        info.append("Team Name: \(profile!.teamName)")
+        info.append("Profile Expiry: \(dateFormatter.string(from: profile!.expirationDate))")
+        
         for (n, certificate) in profile!.developerCertificates.enumerated() {
             info.append("Certificate #: \(n+1)")
             info.append("Common Name: \(certificate.certificate!.commonName!)")
-            info.append("Team Name: \(certificate.certificate!.orgUnit)")      
+            info.append("Team Identifier: \(certificate.certificate!.orgUnit)")      
             info.append("Expiry: \(dateFormatter.string(from: certificate.certificate!.notValidAfter))")
         }
         return info
     }
+
+    private static func getEntitlements(from appPath: String) throws -> [String] {
+        let profileFile = try! Folder(path: appPath).file(named: "embedded.mobileprovision")
+        let profileURL  = URL(fileURLWithPath: profileFile.path)
+
+        let data = try Data(contentsOf: profileURL)
+        let profile = try ProvisioningProfile.parse(from: data)
+
+        let entitlements = profile!.entitlements
+        var info: [String] = []
+        
+        return info
+    }
+
 }
